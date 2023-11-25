480010547522628a81545afc78c198c687dbdc32
AjayKumarRamesh <ajaykumarramesh5@gmail.com>
2023-11-25 21:33:45 +0530

    .

--- deploy/tf_azure/main.tf
+++ deploy/tf_azure/main.tf
@@ -1,10 +1,12 @@
 
+
 resource "azurerm_resource_group" "aks_rg"{
     name = "myAksResourceGroup"
     location = "East US"
 
-depends_on = [ azurerm_resource_group.sa, azurerm_storage_account.sa_qtst, azurerm_storage_container.statefile]
 }
+
+
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name = "myAKSCluster"
@@ -27,7 +29,6 @@ tags = {
     Environment = "Prod"
 }
 
-depends_on = [ azurerm_resource_group.aks_rg ]
 }
 
 
@@ -35,30 +36,4 @@ output "kube_config" {
     value = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config_raw
     sensitive = true
 }
-
-
-
-resource "azurerm_resource_group" "sa" {
-    name = "sa"
-    location = "East US"
-  
-}
-
-resource "azurerm_storage_account" "sa_qtst" {
-    name = "qtst"
-    resource_group_name = azurerm_resource_group.sa.name
-    location = azurerm_resource_group.sa.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-depends_on = [ azurerm_resource_group.sa ]
-}
-
-resource "azurerm_storage_container" "statefile" {
-    name = "tfstatfile"
-    storage_account_name = azurerm_storage_account.sa_qtst.name
-    container_access_type = "public"
-
-  depends_on = [ azurerm_storage_account.sa_qtst ]
-}
-
 
